c0f179b1ccc36305e54553c954351791714990ec
Nguyễn Văn KIên <94183591+nguyenkien0703@users.noreply.github.com>
2022-08-02 00:42:56 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Yêu mình đi",
+    introTitle: '"...Tên đứa xui xẻo bị Sơn xử lý..."',
+    introDesc: `Trước khi học lập trình, anh là người hướng nội. Sau khi học lập trình anh là người hướng đối tượng. Và sau khi quen em, anh chuyển sang hướng về em.`,
+    btnIntro: 'Tiếp nào',
+    title: 'Yêu mừn đi',
+    desc: 'Đi mà=)))))))))))',
+    btnYes: 'Vâng, mình đồng ý <33',
+    btnNo: 'Không nha, chúng ta sẽ làm bạn :3',
+    question: 'Thế giới có 7 tỉ người, tại sao bạn lại thích mình?',
+    btnReply: 'Gửi <3',
+    reply: 'Vì chúng ta sinh ra để dành cho nhau <333',
+    mess: 'Mình biết mà. Yêu bạn nhiều',
+    messDesc: 'Nhắn tin với mình thôi nào',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100034705280584'
+}
